af465e7187c88d248b1568d9169a755750b4dfd2
DarBaala <joker.milan1997gmail.com>
2022-05-16 23:15:23 +0300

    added adaptive layout

---
+++ src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import axios from "axios";
+
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import PizzaBlock from "../components/PizzaBlock/PizzaBlock";
+import Skeleton from "../components/PizzaBlock/Skeleton";
+
+function Home() {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      try {
+        await axios
+          .get("https://62824b53ed9edf7bd8821d30.mockapi.io/items")
+          .then((obj) => setItems(obj.data));
+      } catch (error) {
+        console.error("NOT GET ITEMS");
+      }
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </>
+  );
+}
+export default Home;
